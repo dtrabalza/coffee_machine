0d0a4f951973193abbd115b1fb5c3230c790bb1d
Daniele Trabalza <daniele.trabalza@mideye.com>
2015-11-27 20:28:04 +0100

    Add log

--- src/main/java/com/coffee/CoffeeMachine.java
+++ src/main/java/com/coffee/CoffeeMachine.java
@@ -1,10 +1,17 @@
 package com.coffee;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CoffeeMachine {
 
+	private static Logger logger = LoggerFactory.getLogger(CoffeeMachine.class);
+
 	public static void main(String[] args) {
+		logger.info("Coffee machine started");
 		System.out.println("Super cool coffee machine ready to use!");
 
+		logger.info("Coffee machine ready");
 	}
 
 }
